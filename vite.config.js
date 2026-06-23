7c0a3365436862de2a60215aac50b0b10298067b
Nathanael <nathanael.vinicius03@gmail.com>
2026-06-23 14:34:31 -0300

    fix: corrigindo nome e caminho dos ícones do PWA

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      includeAssets: [],
       manifest: {
         name: 'Sistema de Controle Financeiro',
         short_name: 'Finanças',
