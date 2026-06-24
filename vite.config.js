980ebbff94620695bb75fcf2f9ea1c4b5f0573fd
Nathanael <nathanael.vinicius03@gmail.com>
2026-06-23 22:36:31 -0300

    feat: corrigindo importacao do VitePWA

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite' // <-- Importando o Tailwind v4 obrigatório
-// import { VitePWA } from 'vite-plugin-pwa'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
